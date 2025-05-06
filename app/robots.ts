60236cb4505012f614872d2b9a350ea9123f4931
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2025-05-06 19:15:32 -0400

    fix: :bug: google index disabled by client

--- app/robots.ts
+++ app/robots.ts
@@ -6,16 +6,16 @@ export default function robots(): MetadataRoute.Robots {
       // Regla general para todos los agentes
       {
         userAgent: "*",
-        allow: ["/", "/es/", "/en/"], // Permitir las rutas principales y localizadas
-        disallow: ["/admin/", "/private/", "/api/"], // Bloquear rutas sensibles
+        // allow: ["/", "/es/", "/en/"], // Permitir las rutas principales y localizadas
+        disallow: ["/admin/", "/private/", "/api/", "/", "/es/", "/en/"], // Bloquear rutas sensibles
       },
       // Regla específica para Googlebot (opcional)
       {
         userAgent: "Googlebot",
-        allow: ["/", "/es/", "/en/"],
-        disallow: ["/admin/", "/private/", "/api/"],
+        // allow: ["/", "/es/", "/en/"], // Permitir las rutas principales y localizadas
+        disallow: ["/admin/", "/private/", "/api/", "/", "/es/", "/en/"],
       },
     ],
-    sitemap: `https://talkandbloom.ca/sitemap.xml`, // Referencia al archivo sitemap
+    // sitemap: `https://talkandbloom.ca/sitemap.xml`, // Referencia al archivo sitemap
   };
 }
